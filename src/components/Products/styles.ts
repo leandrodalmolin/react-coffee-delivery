eca9869f5142ba03c4939d5ca64ecac0f02b356e
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2023-02-09 10:40:31 +0000

    Improve products list responsiveness using auto-fit instead of breakpoints

--- src/components/Products/styles.ts
+++ src/components/Products/styles.ts
@@ -1,21 +1,8 @@
 import styled from 'styled-components'
-import { breakpoint } from '../../styles/breakpoints'
 
 export const ProductsList = styled.div`
   display: grid;
-  grid-template-columns: 1fr;
+  grid-template-columns: repeat(auto-fit, minmax(256px, 1fr));
   gap: 2.5rem 2rem;
   margin: 3.4rem 0;
-
-  @media ${breakpoint.md} {
-    grid-template-columns: repeat(2, minmax(0, 1fr));
-  }
-
-  @media ${breakpoint.lg} {
-    grid-template-columns: repeat(3, minmax(0, 1fr));
-  }
-
-  @media ${breakpoint.xl} {
-    grid-template-columns: repeat(4, minmax(0, 1fr));
-  }
 `
