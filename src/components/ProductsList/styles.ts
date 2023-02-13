11ba34997b951ab09dfdc1aac2c3ae5bdda46c1c
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2023-02-13 14:28:26 +0000

    Add bottom margin to all pages (DefaultLayout)

--- src/components/ProductsList/styles.ts
+++ src/components/ProductsList/styles.ts
@@ -4,5 +4,5 @@ export const ProductsListContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(256px, 1fr));
   gap: 2.5rem 2rem;
-  margin: 3.4rem 0;
+  margin-top: 3.4rem;
 `
--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 
+export const DefaultLayoutContainer = styled.div`
+  margin-bottom: 3.5rem;
+`
+
 export const Wrapper = styled.div`
   max-width: 75rem;
   margin: auto;
