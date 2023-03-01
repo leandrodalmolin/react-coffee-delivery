cda2cba8c8cfaea46fdf02bfa682f311f8269b10
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2023-03-01 15:58:22 +0000

    Refactor - move all geolocation functions into a custom hook

--- src/layouts/DefaultLayout/components/Header/Location/index.tsx
+++ src/layouts/DefaultLayout/components/Header/Location/index.tsx
@@ -1,30 +1,27 @@
 import { MapPin } from 'phosphor-react'
 import { useEffect, useState } from 'react'
+import { useGeolocation } from '../../../../../hooks/useGeolocation'
 import { LocationContainer } from './styles'
-import {
-  getCoords,
-  fetchAddressFromAPI,
-} from '../../../../../utils/geolocation'
 
 export function Location() {
+  const { getCoords, fetchAddressFromAPI } = useGeolocation()
   const [location, setLocation] = useState<string | null>(null)
   const [loading, setLoading] = useState(true)
 
-  async function fetchLocation() {
-    try {
-      const { latitude, longitude } = await getCoords()
-      const address = await fetchAddressFromAPI(latitude, longitude)
-      setLocation(`${address.city}, ${address.country_code.toUpperCase()}`)
-    } catch (error: any) {
-      setLocation('Not found')
-    } finally {
-      setLoading(false)
-    }
-  }
-
   useEffect(() => {
+    async function fetchLocation() {
+      try {
+        const { latitude, longitude } = await getCoords()
+        const address = await fetchAddressFromAPI(latitude, longitude)
+        setLocation(`${address.city}, ${address.country_code.toUpperCase()}`)
+      } catch (error: any) {
+        setLocation('Not found')
+      } finally {
+        setLoading(false)
+      }
+    }
     fetchLocation()
-  }, [])
+  }, [getCoords, fetchAddressFromAPI])
 
   return (
     <LocationContainer>
